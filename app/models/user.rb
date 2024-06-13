df75bb9933f0391f95e79fdd2ee99459e583de1d
Sean Wymer <sean.wymer@tcu.edu>
2024-06-13 13:23:29 -0500

    Set up user, organization and membership models

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    uniqueness: { case_sensitive: false }
+
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+
+  before_validation :strip_extranous_spaces
+
+  has_secure_password
+  validates :password,
+    presence: true,
+    length: { minimum: 8 }
+
+  private
+
+    def strip_extranous_spaces
+      self.name = self.name&.strip
+      self.email = self.email&.strip
+    end
+
+end
